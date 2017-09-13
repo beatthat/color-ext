267df9951090485ee73cc3b320f05b3ca754d974
larrykirschner <larrykirschner@gmail.com>
2017-09-12 18:37:42 -0700

    0.0.1

---
+++ src/color-ext/ColorExt.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace BeatThat
+{
+	/// <summary>
+	/// ext functions for Unity Color
+	/// </summary>
+	public static class ColorExt  
+	{
+		public static Color WithAlpha(this Color c, float a)
+		{
+			return new Color(c.r, c.g, c.b, a);
+		}
+
+	}
+}
