d373df7855e15d4ba220d601c0da1e8ef2629360
larrykirschner <larrykirschner@gmail.com>
2018-05-06 22:57:29 -0700

    changed scope from ape to beatthat

--- Runtime/color-ext/ColorExt.cs
+++ Runtime/color-ext/ColorExt.cs
@@ -16,11 +16,11 @@ namespace BeatThat.ColorExtensions
 		public static string ToHex(this Color c)
 		{
 			using (var a = ArrayPool<byte>.Get (4)) {
-				a.array [0] = (byte)c.r;
-				a.array [1] = (byte)c.g;
-				a.array [2] = (byte)c.b;
-				a.array [3] = (byte)c.a;
-				return BitConverter.ToString (a.array);
+				a.array [0] = (byte)(c.r * 255);
+				a.array [1] = (byte)(c.g * 255);
+				a.array [2] = (byte)(c.b * 255);
+				a.array [3] = (byte)(c.a * 255);
+				return BitConverter.ToString (a.array).Replace("-", "");
 			}
 		}
 
